dd9832284e83b3f61e966278e55b8d67edd79659
aaronhpchan <aaronhpchan@gmail.com>
2022-04-30 23:54:21 -0400

    Update Schema

--- sanity_backend/schemas/schema.js
+++ sanity_backend/schemas/schema.js
@@ -8,11 +8,10 @@ import abouts from './abouts'
 import skills from './skills'
 import contact from './contact'
 
-
 // Give schema to the builder and provide the result to Sanity
 export default createSchema({
   // Name schema
   name: 'default',
   // Concatenate document type to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([projects, abouts, skills, contact]),
+  types: schemaTypes.concat([projects, abouts, skills, contact])
 })
